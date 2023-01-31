e68db913bc25b2d408b3640d828be626271eac8b
eladjmc <eladjmc88@gmail.com>
2023-01-31 11:30:19 +0200

    added task 24.1

---
+++ 24.1/24.1.js
@@ -0,0 +1,18 @@
+// You are given a function, Square, that takes four parameters, a,
+// b, c, d, denoting the length of the square edges.
+// Using prototype properties, add a method to Square named
+// isSquare that prints true if a Square object has equal edges
+// and false if they are unequal.
+
+// Here is the Square function:
+function Square(a, b, c, d) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.d = d;
+}
+
+Square.prototype.isSquare = function () {
+  return (this.a === this.b) && (this.b === this.c) && (this.c === this.d);
+};
+
