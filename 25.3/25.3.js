69e56f6054d2eff2b238624b8f618550392c7ee4
eladjmc <eladjmc88@gmail.com>
2023-02-06 11:11:15 +0200

    added 27.1

--- 25.3/25.3.js
+++ 25.3/25.3.js
@@ -11,9 +11,11 @@ const superHeroes = [wonderWoman, batman];
 function printName() {
   console.log(`my name is ${this.name}`);
 }
+
 // We want to print the all heroes names. Implement the
 // printHeroes function:
 // Note: you cannot change the super heroes objects
+
 function printHeroes(heroes, printFunc) {
   //add your code here
     heroes.forEach(hero => {
